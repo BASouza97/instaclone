d129f8220426e65ec1780beb62219afbeb7dffeb
bruno souza <bruno_augustosjc@hotmail.com>
2022-11-16 21:29:12 -0300

    iniciando o clone do instagram

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+//import {container} from './styles';
+
+export default function App() {
+    return (
+        <view />
+    );
+}
