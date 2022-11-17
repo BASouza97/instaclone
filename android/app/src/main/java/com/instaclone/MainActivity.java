a8f918019ca064edd40472f18da108bdf6bccc13
bruno souza <bruno_augustosjc@hotmail.com>
2022-11-16 21:47:33 -0300

    primeiras mudanças / criação do routes, pages/feed

--- android/app/src/main/java/com/instaclone/MainActivity.java
+++ android/app/src/main/java/com/instaclone/MainActivity.java
@@ -3,6 +3,7 @@ package com.instaclone;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -22,7 +23,12 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new MainActivityDelegate(this, getMainComponentName());
+    return new MainActivityDelegate(this, getMainComponentName()); {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import { CreateAppContainer, CreateStackNavigator } from 'react-navigation'; 
+import Feed from './pages/Feed'
+const Routes = CreateAppContainer(
+    CreateStackNavigator({})
+);
